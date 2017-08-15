9f418c378041a1a9e271fef88c5a33022c690039
Tate, Hongliang Tian <tatetian@gmail.com>
2017-08-16 00:22:46 +0800

    Remove useless files for my purpose

--- usr.bin/gprof/hppa.c
+++
@@ -1,8 +0,0 @@
-/*	$OpenBSD: hppa.c,v 1.4 2015/01/20 04:41:01 krw Exp $	*/
-#include        "gprof.h"
-
-void
-findcall(nltype *parentp, unsigned long p_lowpc, unsigned long p_highpc)
-{
-	/* not supported. */
-}
